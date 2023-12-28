4d2feffc51ab74efa7b316e27fb9d71a25bfad58
devAdityaa <acolydebaian8@gmail.com>
2023-12-28 19:07:31 +0530

    Specified a host for the node server

--- main.js
+++ main.js
@@ -48,6 +48,6 @@ app.get('/stream-video', async (req, res) => {
 });
 
 // Start the server and listen on the specified port
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
